d44959c1f0d38640959bfb11135889ad92af3d8f
Zxgaer <zaixiage@outlook.com>
2022-08-29 22:18:52 +0800

    Android

--- app/src/main/java/com/zxgaer/xgditor/addonEditActivity.kt
+++ app/src/main/java/com/zxgaer/xgditor/addonEditActivity.kt
@@ -1,14 +1,10 @@
 package com.zxgaer.xgditor
 
-import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
-import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
 
 
 class addonEditActivity : AppCompatActivity() {
